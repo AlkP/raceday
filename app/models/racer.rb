43bdb631930d37e9868cb60d53ebfec98cd69685
Dan <dchen12812@gmail.com>
2016-01-30 21:04:21 -0800

    added accessors to racer

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,5 +1,7 @@
 class Racer
   include Mongoid::Document
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
 
   #Shortcut to default database
   def self.mongo_client
@@ -9,5 +11,10 @@ class Racer
   #Returns db collection holding Racer
   def self.collection
   	self.mongo_client['racers']
+  end
+
+  #Finds all rows in collection which can match based on protoype, sort, skip, limit
+  def self.all(prototype={...}, sort={...}, skip=0, limit=nil)
+
   end
 end
